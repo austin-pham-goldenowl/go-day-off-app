b694ea5ca540ac7319d069edb7dfe04f041b5c56
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-02-25 15:07:39 +0700

    [FE] - Uninstall modules "typescript" and "@material-ui/icons"

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-const Calendar = React.lazy(() => import('./components/Calendar'));
+import { LeaveRegForm } from './components/LeavingRegForm';
+
+const Calendar = lazy(() => import('./components/Calendar'));
 
 const MainRouter = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/calendar" component={
-                () => (
-                    <React.Suspense fallback={<div>Loading...</div>}>
-                        <Calendar/>
-                    </React.Suspense>)
-            }/>
-            <Route path="/leaveForm" component={LeaveForm}/>
-        </Switch>
-    </Router>
+	<Router>
+		<Switch>
+			<Route exact path="/" component={Home}/>
+			<Route path="/calendar" component={
+				() => (
+					<Suspense fallback={<div>Loading...</div>}>
+						<Calendar/>
+					</Suspense>
+				)
+			}/>
+			<Route path="/leaveForm" component={LeaveForm}/>
+		</Switch>
+	</Router>
 );
 
 const Home = () => {
-    return (
-        <ol>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/calendar">Calendar</Link></li>
-            <li><Link to="/leaveForm">Leaving Form</Link></li>
-        </ol>
-    );
+	return (
+		<ol>
+			<li><Link to="/">Home</Link></li>
+			<li><Link to="/calendar">Calendar</Link></li>
+			<li><Link to="/leaveForm">Leaving Form</Link></li>
+		</ol>
+	);
 };
 
 const LeaveForm = () => {
-    return (
-        <div>Leaving Form</div>
-    );
+	return (
+		<div>
+			<h2>Leaving Form</h2>
+			<LeaveRegForm/>
+		</div>
+	);
 };
 
 class App extends Component {
   render() {
-    return (
-        <div className="App">
-            <header>
-                <div id="logo">
-                    <span>
-                        Golden Owl<b> Leaves</b>
-                    </span>
-                </div>
-            </header>
-            <main>
-                <MainRouter />
-            </main>
-        </div>
-    );
+	return (
+		<div className="App">
+			<header>
+				<div id="logo">
+					<span>
+						Golden Owl<b> Leaves</b>
+					</span>
+				</div>
+			</header>
+			<main>
+				<MainRouter />
+			</main>
+		</div>
+	);
   }
 }
 
