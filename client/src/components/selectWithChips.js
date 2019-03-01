2b6dd264fb801ee456132b05584270e5b081a023
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-01 19:31:38 +0700

    [FE] - Fix bug - Chip's props `onDelete` not work

--- client/src/components/selectWithChips.js
+++ client/src/components/selectWithChips.js
@@ -8,6 +8,7 @@ import {
   Input,
   InputLabel,
   MenuItem,
+  Button,
   
 } from '@material-ui/core';
 import Icon from '@material-ui/core/Icon';
@@ -30,56 +31,72 @@ const styles = theme => ({
 
 
 class SelectWithChips extends React.Component {
-  state = {
-    selected: [],
-  }
+    state = {
+      selected: [],
+    }
 
   handleChange = (event)=>{
     console.log('Chosen value: ', event.target.value);
     this.setState({
       selected: event.target.value
     });
+    this.props.onChange && this.props.onChange(event.target.value);
   };
+
+  handleDeleteBound = value => event => {
+    console.log(value);
+    const {selected} = this.state;
+    let deleteItem = selected.indexOf(value);
+    selected.splice(deleteItem, 1);
+    this.setState({
+      selected: selected
+    });
+  }
 
   render () {
     const { classes, multiple, label, data } = this.props;
     const { selected } = this.state;
     return (
-        <FormControl className={classes.formControl} >
-          <InputLabel htmlFor="select-chip">{label}</InputLabel>
-          <Select
-            multiple={multiple}
-            value={this.state.selected}
-            onChange={this.handleChange}
-            input={<Input id="select-chip"/>}
-            renderValue={selected => (
-              <div className={classes.chips}>
-                {selected.map(item => (
-                  <Chip 
-                    key={shortid.generate()} 
-                    label={item.label} 
-                    className={classes.chip}/>
-                ))}
-              </div>
-            )}
-          >
-            {data.map(item => (
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="select-multiple-chip">Chip</InputLabel>
+        <Select
+          multiple
+          value={selected}
+          onChange={this.handleChange}
+          input={<Input id="select-multiple-chip" />}
+          renderValue={selected => (
+            <div className={classes.chips}>
+              {selected.map(item => (
+                <Chip 
+                  key={item.value} 
+                  label={item.label} 
+                  className={classes.chip} 
+                  onDelete={this.handleDeleteBound(item)}
+                />
+              ))}
+            </div>
+          )}
+        >
+          {data.map(item => (
               <MenuItem 
                 key={shortid.generate()} 
                 value={item}
               >
               { `${item.label} (${item.value})`}
-              {/* {selected.indexOf(item) !== -1 ? (<Icon>check</Icon>) : null} */}
+              {selected.indexOf(item) !== -1 ? (<Icon>check</Icon>) : null}
               </MenuItem>
             ))}
-          </Select>
-        </FormControl>
+        </Select>
+      </FormControl>
     );
   }
 }
 
 SelectWithChips.propTypes = {
   classes: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default withStyles(styles)(SelectWithChips);
