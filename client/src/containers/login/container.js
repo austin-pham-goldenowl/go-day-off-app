83e6a70d7b0f6f9f1cd71c9b70b1ce4fe1743b2a
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-05 18:02:16 +0700

    Add login screen

---
+++ client/src/containers/login/container.js
@@ -0,0 +1,209 @@
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+import { Link } from 'react-router-dom';
+import {
+  Paper,
+  Button,
+  Input,
+  Snackbar,
+  InputLabel,
+  Typography,
+  CssBaseline,
+  FormControl,
+} from '@material-ui/core';
+
+import { withStyles } from '@material-ui/core/styles';
+import { 
+  NOTIF_SUCCESS,
+  NOTIF_WARNING,
+  NOTIF_ERROR,
+  NOTIF_INFO,
+} from '../../constants/form';
+
+import SnackbarNotifContent from '../../components/SnackbarNotification';
+
+import { userLogin } from '../../apiCalls/loginAPI';
+
+const styles = theme => ({
+    main: {
+      width: 'auto',
+      display: 'block', // Fix IE 11 issue.
+      marginLeft: theme.spacing.unit * 3,
+      marginRight: theme.spacing.unit * 3,
+      [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+        width: 400,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      },
+    },
+    paper: {
+      marginTop: theme.spacing.unit * 8,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+      margin: theme.spacing.unit,
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing.unit,
+      marginBottom: theme.spacing.unit
+    },
+    submit: {
+      marginTop: theme.spacing.unit * 3,
+    },
+    forgotPwdHint: {
+      marginTop: theme.spacing.unit *3,
+    }
+  });
+
+class LoginWithFormik extends React.Component {
+  state = {
+    announce: {
+      show: false,
+      type: NOTIF_INFO,
+      message: 'This is an announcement!'
+    }
+  }
+  handleShowAnnouncement = (message = '', type = 'warning') => {
+    this.setState({
+      announce: {
+        show: true,
+        type: type,
+        message: message,
+      }
+    });
+  }
+  handleHideAnnouncement = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    this.setState(prevState => ({
+      announce: {
+        ...prevState.announce,
+        show: false,
+      }
+    }));
+  }
+  render() {
+    const { classes, initialValues } = this.props;
+      return (
+        <main className={classes.main}>
+          {/** Announcement */}
+          <Snackbar
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            open={this.state.announce.show}
+            autoHideDuration={5000}
+            onClose={this.handleHideAnnouncement}
+          >
+            <SnackbarNotifContent
+              onClose={this.handleHideAnnouncement}
+              variant={this.state.announce.type}
+              message={this.state.announce.message}
+            />
+          </Snackbar>
+          {/** End - Announcement */}
+          <CssBaseline />
+          <Paper className={classes.paper}>
+            <Typography component="h1" variant="h4">
+              Sign in
+            </Typography>
+            <Formik
+              initialValues={initialValues}
+              onSubmit={(values, actions) => {
+                console.log(`values: `, values);
+                userLogin(values)
+                  .then(res => {
+                    console.log('RESPONSE -> Login -> ', res);
+                  })
+                  .catch(err => {
+                    console.log('ERROR -> Login -> Error.name: ', err.name);
+                    console.log('ERROR -> Login -> Error.message: ', err.message);
+                    this.handleShowAnnouncement(err.message, NOTIF_ERROR);
+                  });
+              }}
+            >
+            {({ 
+              errors, 
+              values, 
+              handleReset, 
+              handleSubmit,
+              setFieldValue, 
+              handleChange, 
+              ...formikProps  
+            }) => (
+                <Form className={classes.form}>
+                  <Field name="email" render={({field, form, ...otherProps}) => {
+                    console.log('Email field: ',field);
+                    console.log('Email form: ',form);
+                    console.log('otherProps: ', otherProps);
+                    return (
+                      <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="email">Email</InputLabel>
+                        <Input 
+                          autoFocus
+                          id="email" 
+                          name="email" 
+                          type="email" 
+                          value={field.value} 
+                          autoComplete="email" 
+                          onChange={({target: { value}}) => 
+                            setFieldValue(field.name, value)
+                          }
+                        />
+                      </FormControl>
+                    )
+                  }}/>
+                  <Field name="rawPwd" render={({field, form, ...otherProps}) => {
+                    return (
+                      <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="rawPwd">Password</InputLabel>
+                        <Input 
+                          id="password" 
+                          name="rawPwd" 
+                          type="password" 
+                          value ={field.value}
+                          autoComplete="current-password"
+                          onChange={({target: { value}}) => 
+                            setFieldValue(field.name, value)
+                          }
+                        />
+                      </FormControl>
+                    )
+                  }}/>
+                  <Button 
+                    fullWidth
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    className={classes.submit}
+                    onClick={handleSubmit}
+                  >
+                    Let's GO
+                  </Button>
+
+                  <div className={classes.forgotPwdHint}>
+                    Forgot password? Click <Link to="/forgot-password">here</Link>
+                  </div>
+                </Form>
+              )}
+              </Formik>
+          </Paper>
+        </main>
+      )
+  }
+}
+
+LoginWithFormik.defaultProps = {
+  initialValues: {
+    email: 'awfawf@haah.com', 
+    rawPwd: ''
+  }
+}
+export default withStyles(styles)(LoginWithFormik);
--- client/src/containers/main/App.js
+++ client/src/containers/main/App.js
@@ -2,80 +2,96 @@ import React, { Component, Suspense, lazy } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import AbsenceLetter from '../absenceLetter/container';
 
-
+import Login from '../../containers/login/container';
+const AbsenceLetter = lazy(() => import('../../containers/absenceLetter/container'));
 const Calendar = lazy(() => import('../../components/calendar'));
 const DemoUI = lazy(() => import('../../components/demoUI'));
 
 
 const MainRouter = () => (
-	<Router>
-		<Switch>
-			<Route exact path="/" component={Home}/>
-			<Route path="/calendar" component={
-				() => (
-					<Suspense fallback={<div>Loading...</div>}>
-						<Calendar/>
-					</Suspense>
-				)
-			}/>
-			<Route path="/leaveForm" component={LeaveForm}/>
-			<Route path="/demoUI" component={
-				() => (
-					<Suspense fallback={<div><CircularProgress /></div>}>
-						<DemoUIWrapper/>
-					</Suspense>
-				)
-			}/>
-		</Switch>
-	</Router>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/calendar" component={
+        () => (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Calendar/>
+          </Suspense>
+        )
+      }/>
+      <Route path="/leaveForm" component={
+        () => (
+          <Suspense fallback={<div><CircularProgress /></div>}>
+            <AbsenceLetter/>
+          </Suspense>
+        )
+      }/>
+      <Route path="/demoUI" component={
+        () => (
+          <Suspense fallback={<div><CircularProgress /></div>}>
+            <DemoUIWrapper/>
+          </Suspense>
+        )
+      }/>
+      <Route path="/login" component={LoginForm}/>
+    </Switch>
+  </Router>
 );
 
 const Home = () => {
-	return (
-		<ol>
-			<li><Link to="/">Home</Link></li>
-			<li><Link to="/calendar">Calendar</Link></li>
-			<li><Link to="/leaveForm">Leaving Form</Link></li>
-			<li><Link to="/demoUI">Demo UI</Link></li>
-		</ol>
-	);
+  return (
+    <ol>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/calendar">Calendar</Link></li>
+      <li><Link to="/leaveForm">Leaving Form</Link></li>
+      <li><Link to="/demoUI">Demo UI</Link></li>
+      <li><Link to="/login">Login Form</Link></li>
+    </ol>
+  );
 };
 
 const LeaveForm = () => {
-	return (
-		<div>
-			<h1>Leaving Form</h1>
-			<AbsenceLetter />
-		</div>
-	);
+  return (
+    <div>
+      <h1>Leaving Form</h1>
+      <AbsenceLetter />
+    </div>
+  );
 };
 
+const LoginForm = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
 const DemoUIWrapper = () => {
-	return (
-		<div>
-			<DemoUI/>
-		</div>
-	)
+  return (
+    <div>
+      <DemoUI/>
+    </div>
+  )
 }
 
 class App extends Component {
   render() {
-	return (
-		<div className="App">
-			<header>
-				<div id="logo">
-					<span>
-						Golden Owl<b> Leaves</b>
-					</span>
-				</div>
-			</header>
-			<main>
-				<MainRouter />
-			</main>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header>
+        <div id="logo">
+          <span>
+            Golden Owl<b> Leaves</b>
+          </span>
+        </div>
+      </header>
+      <main>
+        <MainRouter />
+      </main>
+    </div>
+  );
   }
 }
 
