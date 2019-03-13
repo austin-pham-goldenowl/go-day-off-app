9228cdc1a77d8f1e3b248bd1130a71fd57ea8729
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-14 00:38:00 +0700

    [FE] - Refactor code

--- client/src/containers/SignOut/container.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import { signOut } from '../../helpers/authHelpers';
-
-export default () => {
-  signOut();
-  return <Redirect to='/login' />
-}
