2b0ac9cddf7bbebb1512237f08ebcb8bdc09b6e2
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-13 15:35:24 +0700

    [FE] - Add api calls: `createLeaveLetter`, `updateLetterStatus`, `getAllApprover`, `getAllInformTo`

---
+++ client/src/constants/leaveTypes.js
@@ -0,0 +1,4 @@
+export const LEAVE_TYPE_PERSONNEL = 1;
+export const LEAVE_TYPE_SICKNESS = 2;
+export const LEAVE_TYPE_ANNUAL = 3;
+export const LEAVE_TYPE_POLICY = 4;
--- client/src/helpers/leaveLetterHelper.js
+++ client/src/helpers/leaveLetterHelper.js
@@ -3,8 +3,21 @@ import {
   LEAVE_REQUEST_APPROVED,
   LEAVE_REQUEST_REJECTED
 } from '../constants/requestStatusType';
+
+import {
+  LEAVE_TYPE_PERSONNEL,
+  LEAVE_TYPE_ANNUAL,
+  LEAVE_TYPE_SICKNESS,
+  LEAVE_TYPE_POLICY
+} from '../constants/leaveTypes';
+
 import PropTypes from 'prop-types';
-const leaveTypesDescription = {1: 'Việc cá nhân', 2: 'Nghỉ ốm', 3: 'Nghỉ phép năm', 4: 'Nghỉ chế độ'};
+const leaveTypesDescription = {
+  [LEAVE_TYPE_PERSONNEL]: 'Việc cá nhân',
+  [LEAVE_TYPE_SICKNESS]: 'Nghỉ ốm',
+  [LEAVE_TYPE_ANNUAL]: 'Nghỉ phép năm', 
+  [LEAVE_TYPE_POLICY]: 'Nghỉ chế độ'
+};
 const leaveReqStatusDescription = {
   [LEAVE_REQUEST_PENDING]: 'Pending',
   [LEAVE_REQUEST_APPROVED]: 'Approved',
@@ -19,9 +32,24 @@ export const getLeaveReqStatus = (typeCode) => {
   return leaveReqStatusDescription[typeCode];
 }
 
+export const getAllLeaveTypes = () => {
+  let size = Object.keys(leaveTypesDescription).length;
+  let leaveTypeArr = [];
+  if(!size) return null;
+  for (let i = 0; i < size; i++) {
+    leaveTypeArr.push({
+      value: i+1,
+      label: leaveTypesDescription[i+1]
+    })
+  }
+  console.log('All leave types: ', leaveTypeArr);
+  return leaveTypeArr;
+}
+
 getLeaveType.propTypes = {
   typeCode: PropTypes.number.isRequired,
 }
 getLeaveReqStatus.propTypes = {
   typeCode: PropTypes.number.isRequired
 }
+
