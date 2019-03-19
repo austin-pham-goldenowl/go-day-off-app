9152246241340f4d898f5fb538f81f83f0c062f7
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-19 14:54:01 +0700

    [FE] - LetterManagement - Change DateTime displaying format to `DD/MM/YYYY`

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,2 +1,6 @@
 export const SERVER_HOST_DEV = 'http://localhost:3001/api/v1.0.0';
+<<<<<<< HEAD
 export const SERVER_HOST_PROD = '';
+=======
+export const SERVER_HOST_PROD = '';
+>>>>>>> [FE] - LetterManagement - Change DateTime displaying format to `DD/MM/YYYY`
