1c01e9227efa572d85b3e5d24de5364ac8f1356e
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-20 11:59:52 +0700

    [FE] - DEBUG

--- server/controllers/v1.0.0/user.js
+++ server/controllers/v1.0.0/user.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const Router = express.Router();
 
 /**
@@ -9,7 +9,7 @@ const {
   positions: positionModel,
   teams: teamModel,
   userPermission: permissionModel
-} = require("../../models");
+} = require('../../models');
 
 /**
  * Helpers
@@ -17,12 +17,12 @@ const {
 const {
   handleSuccess,
   handleFailure
-} = require("../../helpers/handleResponse");
-const { standardizeObj } = require("../../helpers/standardize");
+} = require('../../helpers/handleResponse');
+const { standardizeObj } = require('../../helpers/standardize');
 const {
   getIdFromToken,
   getPermissionByUserId
-} = require("../../helpers/getUserInfo");
+} = require('../../helpers/getUserInfo');
 
 /**
  * Middlewares
@@ -31,48 +31,48 @@ const userMustBeHR = require("../../middlewares/userMustBeHR");
 const bodyMustNotEmpty = require('../../middlewares/bodyMustNotEmpty');
 
 // Get user profile
-Router.get("/profile", async (req, res) => {
+Router.get('/profile', async (req, res) => {
   try {
     const ownUserId = getIdFromToken(req.token_payload);
-    if (!ownUserId) throw { msg: "USER_NOT_FOUND" };
+    if (!ownUserId) throw { msg: 'USER_NOT_FOUND' };
     const demandUserId = ownUserId;
-    if (!demandUserId) throw { msg: "USER_NOT_FOUND" };
+    if (!demandUserId) throw { msg: 'USER_NOT_FOUND' };
 
     // HR can view profile of everyone
     // Others can view oneself's
     const fUserType = await getPermissionByUserId(ownUserId);
-    if (!fUserType || (fUserType !== "HR" && ownUserId !== demandUserId))
-      throw { code: 401, msg: "NO_PERMISSION" };
+    if (!fUserType || (fUserType !== 'HR' && ownUserId !== demandUserId))
+      throw { code: 401, msg: 'NO_PERMISSION' };
 
     const attributes = [
-      "fEmail",
-      "fFirstName",
-      "fLastName",
-      "fPhone",
-      "fPosition",
-      "fTeamId",
-      "fTypeId",
-      "fUsername",
-      "fGender"
+      'fEmail',
+      'fFirstName',
+      'fLastName',
+      'fPhone',
+      'fPosition',
+      'fTeamId',
+      'fTypeId',
+      'fUsername',
+      'fGender'
     ];
     const users = await userModel.loadAll(attributes, {
       where: { fId: demandUserId }
     });
-    if (!users || users.length !== 1) throw { msg: "USER_NOT_FOUND" };
+    if (!users || users.length !== 1) throw { msg: 'USER_NOT_FOUND' };
     //extract info
     const user = users[0].get({ plain: true });
     const { fPosition, fTeamId } = user;
     // get position name
-    const positions = await positionModel.loadAll(["fPosName"], {
+    const positions = await positionModel.loadAll(['fPosName'], {
       where: { fId: fPosition }
     });
-    if (!positions || positions.length !== 1) throw { msg: "USER_NOT_FOUND" };
+    if (!positions || positions.length !== 1) throw { msg: 'USER_NOT_FOUND' };
     user.fPositionName = positions[0].get({ plain: true }).fPosName;
     // get team name
-    const teams = await teamModel.loadAll(["fTeamName"], {
+    const teams = await teamModel.loadAll(['fTeamName'], {
       where: { fId: fTeamId }
     });
-    if (!teams || teams.length !== 1) throw { msg: "USER_NOT_FOUND" };
+    if (!teams || teams.length !== 1) throw { msg: 'USER_NOT_FOUND' };
     user.fTeamName = teams[0].get({ plain: true }).fTeamName;
 
     handleSuccess(res, { user });
@@ -85,15 +85,15 @@ Router.get("/profile", async (req, res) => {
 Router.patch("/profile", bodyMustNotEmpty, userMustBeHR, async (req, res) => {
   try {
     const userId = getIdFromToken(req.token_payload);
-    if (!userId) throw { msg: "USER_NOT_FOUND" };
+    if (!userId) throw { msg: 'USER_NOT_FOUND' };
 
     const keys = Object.keys(req.body);
     if (
       keys.length < 2 ||
-      !keys.includes("info") ||
-      (keys.includes("info") && Object.keys(req.body.info) < 1)
+      !keys.includes('info') ||
+      (keys.includes('info') && Object.keys(req.body.info) < 1)
     )
-      throw { msg: "INVALID_VALUES" };
+      throw { msg: 'INVALID_VALUES' };
     const entity = req.body.info && standardizeObj(req.body.info);
 
     // validate gender value
@@ -102,7 +102,7 @@ Router.patch("/profile", bodyMustNotEmpty, userMustBeHR, async (req, res) => {
       (fGender || 3) &&
       !userModel.rawAttributes.fGender.values.includes(fGender)
     )
-      throw { msg: "INVALID_VALUES" };
+      throw { msg: 'INVALID_VALUES' };
 
     // update foreign keys
     const { fTeamId, fPositionId, fTypeId } = entity;
@@ -113,7 +113,7 @@ Router.patch("/profile", bodyMustNotEmpty, userMustBeHR, async (req, res) => {
     const affected = await userModel.modify(entity, {
       where: { fId: userId }
     });
-    if (affected[0] !== 1) throw { msg: "USER_NOT_FOUND" };
+    if (affected[0] !== 1) throw { msg: 'USER_NOT_FOUND' };
 
     handleSuccess(res, { user: entity });
   } catch (err) {
@@ -121,18 +121,18 @@ Router.patch("/profile", bodyMustNotEmpty, userMustBeHR, async (req, res) => {
   }
 });
 
-Router.get("/approver", async (req, res) => {
+Router.get('/approver', async (req, res) => {
   try {
     // get hr user type id
-    const userTypeIds = await permissionModel.loadAll(["fId"], {
-      where: { fUserType: "HR" }
+    const userTypeIds = await permissionModel.loadAll(['fId'], {
+      where: { fUserType: 'HR' }
     });
     if (!userTypeIds || userTypeIds.length !== 1)
-      throw { msg: "USER_NOT_FOUND" };
+      throw { msg: 'USER_NOT_FOUND' };
 
     // get users who are hr
     const hrId = userTypeIds[0].get({ plain: true }).fId;
-    const users = await userModel.loadAll(["fId", "fFirstName", "fLastName"], {
+    const users = await userModel.loadAll(['fId', 'fFirstName', 'fLastName'], {
       where: { fTypeId: hrId }
     });
     const approvers = users.map(user => user.get({ plain: true }));
@@ -143,28 +143,37 @@ Router.get("/approver", async (req, res) => {
   }
 });
 
-Router.get("/team-leader", async (req, res) => {
+Router.get('/team-leader', async (req, res) => {
   try {
+    console.log(`userController -> path '/team-leader':`);
     const { userId } = req.token_payload;
     // find which team the user belongs to
-    const users = await userModel.loadAll(["fTeamId"], {
+    const users = await userModel.loadAll(['fTeamId'], {
       where: { fId: userId }
     });
-    if (!users || users.length !== 1) throw { msg: "USER_NOT_FOUND" };
+    if (!users || users.length !== 1) throw { msg: 'USER_NOT_FOUND' };
+    console.log(`[PASS] 1`);
 
     // find which user is the team leader
     const { fTeamId } = users[0].get({ plain: true });
-    const teams = await teamModel.loadAll(["fTeamLead"], {
+    const teams = await teamModel.loadAll(['fTeamLead'], {
       where: { fId: fTeamId }
     });
-    if (!teams || teams.length !== 1) throw { msg: "USER_NOT_FOUND" };
+    console.log(`-> teams: `, teams);
+    console.log(`-> teams.length: `, teams.length);
+    if (!teams || teams.length !== 1) throw { msg: 'USER_NOT_FOUND' };
+    console.log(`[PASS] 2`);
 
     // find the team leader name
     const { fTeamLead } = teams[0].get({ plain: true });
-    const leaders = await userModel.loadAll(["fFirstName", "fLastName"], {
+    console.log(`-> fTeamLead: `, fTeamLead);
+    const leaders = await userModel.loadAll(['fFirstName', 'fLastName'], {
       where: { fId: fTeamLead }
     });
-    if (!leaders || leaders.length !== 1) throw { msg: "USER_NOT_FOUND" };
+    console.log(`-> leaders: `, leaders);
+    console.log(`-> leaders.length: `, leaders.length);
+    if (!leaders || leaders.length !== 1) throw { msg: 'LEADERS_NOT_FOUND' };
+    console.log(`[PASS] 3`);
 
     const { fFirstName, fLastName } = leaders[0].get({ plain: true });
     const teamLeader = {
