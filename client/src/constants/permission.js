94492a1ececd3a817b98af1d939d6564484f4dcc
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-20 09:38:21 +0700

    [FE] - Hot fix - Redefine UserPermission values

--- client/src/constants/permission.js
+++ client/src/constants/permission.js
@@ -1,10 +1,10 @@
 const userTypes = {
-  MODE_PERSONNEL: "personnel",
-  MODE_ADMIN: "administration",
-  MODE_HR: "hr"
+  MODE_PERSONNEL: 'personnel',
+  MODE_ADMIN: 'administration',
+  MODE_HR: 'hr'
 };
 export const responseUserPermission = {
-  USER: "gbAiv",
-  HR: "3sVfP"
+  USER: '3sVfP',
+  HR: 'NH6Bs'
 };
 export default userTypes;
