bdbef2a570cb164af5e7150bbe0b34111e5ac446
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-21 16:12:23 +0700

    [FE] - Refactor code

--- client/src/apiCalls/leaveLetterAPI.js
+++ client/src/apiCalls/leaveLetterAPI.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import axios from 'axios';
 import { getCookie } from 'tiny-cookie';
 
@@ -16,19 +17,32 @@ import { getUserId } from '../helpers/authHelpers';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.patch['Content-Type'] = 'application/json';
 
+=======
+import axios from 'axios'
+import { getCookie } from 'tiny-cookie'
+import { SERVER_HOST_DEV } from '../constants/api'
+import { ACCESS_TOKEN_KEY } from '../constants/token'
+
+import { getUserId } from '../helpers/authHelpers'
+
+// General header params for some methods
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.patch['Content-Type'] = 'application/json'
+
+>>>>>>> [FE] - Refactor code
 export const getLeaveLetterDetails = id =>
   axios.get(`${SERVER_HOST_DEV}/leaveLetter/details?id=${id}`, {
     headers: {
       'x-access-token': getCookie(ACCESS_TOKEN_KEY)
     }
-  });
+  })
 
 export const getAllLeaveLetters = (size = 10, page = 1) =>
   axios.get(`${SERVER_HOST_DEV}/leaveLetter?page=${page}&size=${size}`, {
     headers: {
       'x-access-token': getCookie(ACCESS_TOKEN_KEY)
     }
-  });
+  })
 
 export const getMyLeaveLetters = (size = 10, page = 1, demandUserId = getUserId()) =>
   axios.get(`${SERVER_HOST_DEV}/leaveLetter/my-letters?userId=${demandUserId}&page=${page}&size=${size}`, {
@@ -59,16 +73,17 @@ export const createLeaveLetter = letterEntity => {
 
 export const updateLetterStatus = (letterId, userId, statusKey) => {
   return axios.patch(`${SERVER_HOST_DEV}/leaveletter`, 
-  {
-    'id': letterId,
-    'info': {
-      'status': statusKey,
-      'userId': userId,
+    {
+      'id': letterId,
+      'info': {
+        'status': statusKey,
+        'userId': userId,
+      }
     },
-  },
-  {
-    headers: {
-      'x-access-token': getCookie(ACCESS_TOKEN_KEY)
+    {
+      headers: {
+        'x-access-token': getCookie(ACCESS_TOKEN_KEY)
+      }
     }
-  })
-};
+  );
+}
--- client/src/components/RequestStatusPill.js
+++ client/src/components/RequestStatusPill.js
@@ -1,46 +1,44 @@
 import React from 'react';
 import classNames from 'classnames';
-import {
-  Chip,
-  Avatar
-} from '@material-ui/core';
+import { Chip, Avatar } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import {
   AccessTime as PendingIcon,
   DoneAll as ApprovedIcon,
   HighlightOffOutlined as RejectIcon,
-  ErrorOutline as ErrorIcon,
+  ErrorOutline as ErrorIcon
 } from '@material-ui/icons';
-import * as ReqStatusType from '../constants/requestStatusType';
 
-import { 
-  getLeaveReqStatus,
-} from '../helpers/leaveLetterHelper';
+//constants
+import * as ReqStatusType from '../constants/requestStatusType';
+import { requestStatusPillColors as PillColors } from '../constants/appThemeColors';
+//helpers
+import { getLeaveReqStatus } from '../helpers/leaveLetterHelper';
 
 const styles = theme => ({
   pendingColor: {
-    color: '#9F6000',
+    color: '#9F6000'
   },
   approvedColor: {
-    color: '#4F8A10',
+    color: '#4F8A10'
   },
   rejectedColor: {
-    color: '#D8000C',
+    color: '#D8000C'
   },
   pending: {
     fontWeight: 'bold',
-    backgroundColor: '#FEEFB3'
+    backgroundColor: PillColors.pending
   },
   approved: {
     fontWeight: 'bold',
-    backgroundColor: '#DFF2BF'
+    backgroundColor: PillColors.approved
   },
   rejected: {
     fontWeight: 'bold',
-    backgroundColor: '#FFD2D2'
+    backgroundColor: PillColors.rejected
   },
-  error:{
+  error: {
     backgroundColor: '#C1C1C1'
   }
 });
@@ -52,42 +50,44 @@ const styles = theme => ({
  * 2: Reject
  */
 
-const RequestStatus = (props) => {
+const RequestStatus = props => {
   const { classes, statusType } = props;
   switch (statusType) {
     case ReqStatusType.LEAVE_REQUEST_PENDING:
       return (
-        <Chip 
+        <Chip
           label={getLeaveReqStatus(statusType)}
-          className={classNames(classes.pending, classes.pendingColor)} 
-          avatar={<PendingIcon className={classes.pendingColor}/>} 
+          className={classNames(classes.pending, classes.pendingColor)}
+          avatar={<PendingIcon className={classes.pendingColor} />}
         />
-      )
+      );
     case ReqStatusType.LEAVE_REQUEST_APPROVED:
       return (
-        <Chip 
-          label='Approved' 
-          className={classNames(classes.approved, classes.approvedColor)} 
-          avatar={<ApprovedIcon className={classes.approvedColor}/>}
+        <Chip
+          label="Approved"
+          className={classNames(classes.approved, classes.approvedColor)}
+          avatar={<ApprovedIcon className={classes.approvedColor} />}
         />
-      )
+      );
     case ReqStatusType.LEAVE_REQUEST_REJECTED:
       return (
-        <Chip 
-          label='Rejected'
-          className={classNames(classes.rejected, classes.rejectedColor)} 
-          avatar={<RejectIcon className={classes.rejectedColor}/>}
+        <Chip
+          label="Rejected"
+          className={classNames(classes.rejected, classes.rejectedColor)}
+          avatar={<RejectIcon className={classes.rejectedColor} />}
         />
-      )
+      );
     default:
       return (
-        <Avatar><ErrorIcon /></Avatar>
-        )
+        <Avatar>
+          <ErrorIcon />
+        </Avatar>
+      );
   }
-}
+};
 
 RequestStatus.propTypes = {
-  statusType: PropTypes.number.isRequired,
-}
+  statusType: PropTypes.number.isRequired
+};
 
 export default withStyles(styles)(RequestStatus);
