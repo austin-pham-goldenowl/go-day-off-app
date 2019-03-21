131021acbfb4842f9131419c2307bc87130e3f01
nolan <nolan.trhdon123@gmail.com>
2019-03-21 14:51:22 +0700

    Added pagination for letterManagement

--- client/src/apiCalls/leaveLetterAPI.js
+++ client/src/apiCalls/leaveLetterAPI.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 import { getCookie } from "tiny-cookie";
+
+/**
+ * Constants
+ */
 import { SERVER_HOST_DEV } from "../constants/api";
 import { ACCESS_TOKEN_KEY } from "../constants/token";
 
+/**
+ * Helpers
+ */
 import { getUserId } from '../helpers/authHelpers';
 
-//General header params for some methods
+// General header params for some methods
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.patch['Content-Type'] = 'application/json';
-
-
 
 export const getLeaveLetterDetails = id =>
   axios.get(`${SERVER_HOST_DEV}/leaveLetter/details?id=${id}`, {
@@ -18,19 +23,20 @@ export const getLeaveLetterDetails = id =>
     }
   });
 
-export const getAllLeaveLetters = () =>
-  axios.get(`${SERVER_HOST_DEV}/leaveLetter/`, {
+export const getAllLeaveLetters = (page = 1, size = 10) =>
+  axios.get(`${SERVER_HOST_DEV}/leaveLetter?page=${page}&size=${size}`, {
     headers: {
       "x-access-token": getCookie(ACCESS_TOKEN_KEY)
     }
   });
 
-export const getMyLeaveLetters = () =>
-  axios.get(`${SERVER_HOST_DEV}/leaveLetter/my-letters`, {
+export const getMyLeaveLetters = (page = 1, size = 10, demandUserId = getUserId()) =>
+  axios.get(`${SERVER_HOST_DEV}/leaveLetter/my-letters?userId=${demandUserId}&page=${page}&size=${size}`, {
     headers: {
       "x-access-token": getCookie(ACCESS_TOKEN_KEY)
     }
   });
+
 export const createLeaveLetter = (letterEntity) => {
   return axios.post(`${SERVER_HOST_DEV}/leaveletter`, 
     {
