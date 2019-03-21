9e4a78ff4059b074841540ab77302a836c04c53a
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-21 16:12:23 +0700

    [FE] - Refactor code

--- client/src/apiCalls/leaveLetterAPI.js
+++ client/src/apiCalls/leaveLetterAPI.js
@@ -1,47 +1,49 @@
-import axios from "axios";
-import { getCookie } from "tiny-cookie";
-import { SERVER_HOST_DEV } from "../constants/api";
-import { ACCESS_TOKEN_KEY } from "../constants/token";
+import axios from 'axios'
+import { getCookie } from 'tiny-cookie'
+import { SERVER_HOST_DEV } from '../constants/api'
+import { ACCESS_TOKEN_KEY } from '../constants/token'
 
-import { getUserId } from '../helpers/authHelpers';
+import { getUserId } from '../helpers/authHelpers'
 
-//General header params for some methods
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.headers.patch['Content-Type'] = 'application/json';
-
-
+// General header params for some methods
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.patch['Content-Type'] = 'application/json'
 
 export const getLeaveLetterDetails = id =>
   axios.get(`${SERVER_HOST_DEV}/leaveLetter/details?id=${id}`, {
     headers: {
-      "x-access-token": getCookie(ACCESS_TOKEN_KEY)
+      'x-access-token': getCookie(ACCESS_TOKEN_KEY)
     }
-  });
+  })
 
 export const getAllLeaveLetters = () =>
   axios.get(`${SERVER_HOST_DEV}/leaveLetter/`, {
     headers: {
-      "x-access-token": getCookie(ACCESS_TOKEN_KEY)
+      'x-access-token': getCookie(ACCESS_TOKEN_KEY)
     }
-  });
+  })
 
 export const getMyLeaveLetters = () =>
   axios.get(`${SERVER_HOST_DEV}/leaveLetter/my-letters`, {
     headers: {
-      "x-access-token": getCookie(ACCESS_TOKEN_KEY)
+      'x-access-token': getCookie(ACCESS_TOKEN_KEY)
     }
-  });
-export const createLeaveLetter = (letterEntity) => {
-  return axios.post(`${SERVER_HOST_DEV}/leaveletter`, 
+  })
+export const createLeaveLetter = letterEntity => {
+  return axios.post(
+    `${SERVER_HOST_DEV}/leaveletter`,
     {
-      "absenceType": letterEntity.leaveType,
-      "fromDT": letterEntity.startDate,
-      "toDT": letterEntity.endDate,
-      "status": 1,
-      "substituteId": 'i53FItHeMK',
-      "userId": getUserId(),
-      "approver": letterEntity.approver,
-      "reason": letterEntity.otherReason !== "" ? letterEntity.otherReason : letterEntity.reason
+      absenceType: letterEntity.leaveType,
+      fromDT: letterEntity.startDate,
+      toDT: letterEntity.endDate,
+      status: 1,
+      substituteId: 'i53FItHeMK',
+      userId: getUserId(),
+      approver: letterEntity.approver,
+      reason:
+        letterEntity.otherReason !== ''
+          ? letterEntity.otherReason
+          : letterEntity.reason
     },
     {
       headers: {
@@ -52,17 +54,19 @@ export const createLeaveLetter = (letterEntity) => {
 }
 
 export const updateLetterStatus = (letterId, userId, statusKey) => {
-  return axios.patch(`${SERVER_HOST_DEV}/leaveletter`, 
-  {
-    "id": letterId,
-    "info": {
-      "status": statusKey,
-      "userId": userId,
+  return axios.patch(
+    `${SERVER_HOST_DEV}/leaveletter`,
+    {
+      id: letterId,
+      info: {
+        status: statusKey,
+        userId: userId
+      }
     },
-  },
-  {
-    headers: {
-      'x-access-token': getCookie(ACCESS_TOKEN_KEY)
+    {
+      headers: {
+        'x-access-token': getCookie(ACCESS_TOKEN_KEY)
+      }
     }
-  })
+  )
 }
--- client/src/constants/appThemeColors/index.js
+++ client/src/constants/appThemeColors/index.js
@@ -1,1 +1,9 @@
 export const appBarColor = '#FFBE00';
+export const formValidateError = '';
+
+export const requestStatusPillColors = {
+  approved: '#DFF2BF',
+  rejected: '#FFD2D2',
+  pending: '#FEEFB3',
+  onLoad: '#C1C1C1'
+};
