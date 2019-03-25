2f1e4a2c6fa128b16daa8bccb7ddd7aea5ac5614
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-25 11:37:55 +0700

    [FE] - Move `Mockup data` to a separated file

---
+++ client/src/constants/mockups/index.js
@@ -0,0 +1,24 @@
+
+// Mockup data
+export const mockupLeaveLetterReasons = [
+  {
+    value: 'Bị ốm',
+    label: 'Bị ốm'
+  },
+  {
+    value: 'Giải quyết việc gia đình',
+    label: 'Giải quyết việc gia đình'
+  },
+  {
+    value: 'Có lịch hẹn khám bệnh',
+    label: 'Có lịch hẹn khám bệnh'
+  },
+  {
+    value: 'Áp lực công việc',
+    label: 'Áp lực công việc'
+  },
+  {
+    value: 'Lý do khác',
+    label: 'Lý do khác'
+  }
+];
