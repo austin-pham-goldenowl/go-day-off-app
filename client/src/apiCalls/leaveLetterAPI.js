5bd207d32e80bcb93765c33e06c290856774024f
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-31 23:41:42 +0700

    [FE] - APIs - Separate business logic for "rejectLeaveLetter" and "approveLeaveLetter"

--- client/src/apiCalls/leaveLetterAPI.js
+++ client/src/apiCalls/leaveLetterAPI.js
@@ -1,15 +1,25 @@
 import axios from 'axios'
+import PropTypes from 'prop-types'
 import { getCookie } from 'tiny-cookie'
-import { SERVER_HOST_DEV } from '../constants/api'
-import { ACCESS_TOKEN_KEY } from '../constants/token'
-import { LEAVE_REQUEST_PENDING } from '../constants/requestStatusType';
 
-import { getUserId } from '../helpers/authHelpers'
+//constants
+import { SERVER_HOST_DEV } from '../constants/api'
+import { userTypes } from '../constants/permission';
+import { ACCESS_TOKEN_KEY } from '../constants/token';
+import { REJECT_TYPE } from '../constants/rejectType';
+import { 
+  LEAVE_REQUEST_PENDING, 
+  LEAVE_REQUEST_APPROVED,
+} from '../constants/requestStatusType';
+
+//helpers
+import { getUserId, getUserEntity } from '../helpers/authHelpers'
 
 // General header params for some methods
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.headers.patch['Content-Type'] = 'application/json'
 
+// Promise Functions
 export const getLeaveLetterDetails = id =>
   axios.get(`${SERVER_HOST_DEV}/leaveLetter/details?id=${id}`, {
     headers: {
@@ -28,7 +38,7 @@ export const getMyLeaveLetters = (
   page = 1,
   size = 10,
   demandUserId = getUserId()
-) =>
+) => 
   axios.get(
     `${SERVER_HOST_DEV}/leaveLetter/my-letters?userId=${demandUserId}&page=${page}&size=${size}`,
     {
@@ -63,12 +73,30 @@ export const createLeaveLetter = letterEntity => {
   );
 };
 
-export const updateLetterStatus = (letterId, userId, statusKey) => {
+export const rejectLeaveLetterRequest = (letterId, rejectReason) => {
+  //get the permission info
+  let { userType } = getUserEntity();
+
+  return axios.post(`${SERVER_HOST_DEV}/reject`, {
+      letterId: letterId,
+      reason: rejectReason,
+      rejectType: userType && (userType === userTypes.MODE_PERSONNEL 
+        ? REJECT_TYPE.BY_SELF : REJECT_TYPE.BY_APPROVER),
+    },
+    {
+      headers: {
+        'x-access-token': getCookie(ACCESS_TOKEN_KEY)
+      }
+    }
+  );
+}
+
+export const approveLeaveLetterRequest = (letterId, userId) => {
   return axios.patch(`${SERVER_HOST_DEV}/leaveletter`, 
     {
       'id': letterId,
       'info': {
-        'status': statusKey,
+        'status': LEAVE_REQUEST_APPROVED,
         'userId': userId,
       }
     },
